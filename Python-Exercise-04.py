393359ffe50dc5c38505f5b209282ad8fe9c3ae0
DanielQFK <DanialQueen2004@gmail.com>
2021-06-23 00:08:47 -0400

    Numbers...

---
+++ Python-Exercise-04.py
@@ -0,0 +1,28 @@
+# Numbers
+# some exercise about numbers
+
+# 1.
+print(5+7)
+#The output = 12
+
+# 2.
+print(9-19)
+#The output = -10
+
+# 3.
+print(8*6)
+#The output = 48
+
+# 4.
+print(98/45)
+#The output = 2.1777777777777776
+
+# 5.
+print(5**2)
+#The output = 25
+
+# 6.
+print(((242/2)*(8**6))-(143.5*10))
+#The output = 31717989.0
+
+
