b3a6902e0e333f3e498828e47cc7b929c5485243
DanielQFK <DanialQueen2004@gmail.com>
2021-06-28 00:37:39 -0400

    List...

---
+++ Python-Exercise-05.py
@@ -0,0 +1,64 @@
+# list
+# some exercises about List
+Name = ["alex" , "TIM" , "Christian" , "ryan" , "joHN" ]
+
+print(Name)
+# The output would be = ['alex', 'TIM', 'Christian', 'ryan', 'joHN']
+
+print(Name[1].lower())
+# The output would be = tim
+
+print(Name[0].title())
+# The output would be = Alex
+
+print(Name[-1].lower())
+# The output would be = john
+
+print(Name[-2].title())
+# The output would be = Ryan
+
+print(Name[2].upper())
+# The output would be = CHRISTIAN
+
+
+
+
+
+Number = [1 , 2 , 3 , 4 , 5 , 6 , 7 , 8 , 9 , 10]
+print(Number)
+# The output would be = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10] 
+
+print(Number[4]*Number[7])
+# The output would be = 40
+
+print(Number[6]/Number[0])
+# The output would be = 7.0
+
+print(Number[3]**Number[2])
+# The output would be = 64
+
+print(Number[9]+Number[5])
+# The output would be = 16
+
+print(Number[3]-Number[1])
+# The output would be = 2
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
