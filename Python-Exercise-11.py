6315c9b2c953ec1b530c316907b846d584d8a698
DanielQFK <DanialQueen2004@gmail.com>
2021-07-19 03:10:46 -0400

    Tuple

---
+++ Python-Exercise-11.py
@@ -0,0 +1,38 @@
+# Tuple exercises
+
+# 1.
+Tuple1 = ("daniel" , "Max" , "EMMA")
+print(Tuple1)
+# Output = ('daniel', 'Max', 'EMMA')
+
+print(Tuple1.title())
+# Output = Traceback (most recent call last):
+#   File "Python-Exercise-11.py", line 7, in <module>
+#     print(Tuple1.title())
+# AttributeError: 'tuple' object has no attribute 'title'
+print(sorted.Tuple1())
+#Even you can not sort the tuple...
+
+
+# 2.
+Tuple2 = (100)
+print(Tuple2)
+# Output = 100
+
+
+# 3.
+Tuple3 = (23 , 52 , 4)
+print(Tuple3)
+# Output = (23, 52, 4)
+
+
+# 4.
+Tuple4 = ('Max')
+Tuple4.append('Ella')
+print(Tuple4)
+# Output = Traceback (most recent call last):
+#   File "Python-Exercise-11.py", line 31, in <module>
+#     Touple4.append('Ella')
+# AttributeError: 'str' object has no attribute 'append' 
+
+# Tip = As you see touples don't accept any changes! 
