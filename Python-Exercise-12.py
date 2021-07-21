9e9476eb0cd82343a18eff3d767f23873de63aa9
DanielQFK <DanialQueen2004@gmail.com>
2021-07-21 01:33:44 -0400

    loop

---
+++ Python-Exercise-12.py
@@ -0,0 +1,87 @@
+# loop exercises...
+
+# 1.
+for x in range(10):
+    print(x)
+# output = 0
+# 1
+# 2
+# 3
+# 4
+# 5
+# 6
+# 7
+# 8
+# 9
+
+for i in range(1 , 20 , 2):
+    print(i)
+# output = 1
+# 3
+# 5
+# 7
+# 9
+# 11
+# 13
+# 15
+# 17
+# 19
+
+for w in range(0 , 20 , 2):
+    print(w)
+# output =0
+# 2
+# 4
+# 6
+# 8
+# 10
+# 12
+# 14
+# 16
+# 18
+
+for q in range(0 , 20 , 3):
+    print(q)
+# output = 0
+# 3
+# 6
+# 9
+# 12
+# 15
+# 18
+
+
+# 2.
+List = ["daniel" , "laura" , "max" , "misha"]
+for b in List:
+    print(b)
+    print(b.title())
+    print(b.upper())
+# output =daniel
+# Daniel
+# DANIEL
+# laura
+# Laura
+# LAURA
+# max
+# Max
+# MAX
+# misha
+# Misha
+# MISHA
+     
+# 3.
+List1 = [3 , 8 , 345 , 2 , 654 ]
+for c in List1:
+    print(c) 
+    print("Hello...")
+# output =
+# 3
+# Hello...
+# 8
+# Hello...
+# 345
+# Hello...
+# 2
+# Hello...
+# 654
