e8c9118303fac82d16fee8d7253a79971c813565
DanielQFK <DanialQueen2004@gmail.com>
2021-07-26 00:56:53 -0400

    Dictionary

---
+++ Python-Exercise-14.py
@@ -0,0 +1,50 @@
+# Dictionary exercise
+
+# 1
+Dictionary1 = {
+    'c':'Clear' , 
+    'f':'Files' , 
+    'q':'Quit' , 
+    'bg':'Background' , 
+    'p':'print' , 
+    'cc':'Clear Cache' ,
+    'mkdir':'make a directory' ,
+}
+print(Dictionary1)
+# output = {'c': 'Clear', 'f': 'Files', 'q': 'Quit', 'bg': 'Background', 'p': 'print', 'cc': 'Clear Cache', 'mkdir': 'make a directory'}
+
+# 2
+Dictionary1['s'] = 'Screenshot'
+Dictionary1['r'] = 'Reload'
+print(Dictionary1)
+# output = {'c': 'Clear', 'f': 'Files', 'q': 'Quit', 'bg': 'Background', 'p': 'print', 'cc': 'Clear Cache', 'mkdir': 'make a directory', 's': 'Screenshot', 'r': 'Reload'}
+
+# 3
+Dictionary2 = {}
+Dictionary2['A'] = 'a'
+Dictionary2['B'] = 'b'
+Dictionary2['C'] = 'c'
+Dictionary2['D'] = 'd'
+Dictionary2['E'] = 'e'
+Dictionary2['F'] = 'f'
+print(Dictionary2)
+# output = {'A': 'a', 'B': 'b', 'C': 'c', 'D': 'd', 'E': 'e', 'F': 'f'}
+
+# 4
+Dictionary3 = {
+    'Name':'Daniel' ,
+    'Lastname':'TK' ,
+    'Age':17 ,
+    'University':'None'
+}
+print(Dictionary3)
+# output ={'Name': 'Daniel', 'Lastname': 'TK', 'Age': 17, 'University': 'None'} 
+
+Dictionary3['Name'] = 'Max'
+Dictionary3['Lastname'] = 'Amini'
+Dictionary3['Age'] = 33
+Dictionary3['University'] = 'Los Angeles University'
+print(Dictionary3)
+# output = {'Name': 'Max', 'Lastname': 'Amini', 'Age': 33, 'University': 'Los Angeles University'}
+
+
